e3ec1f28a5f738a29ec1e59f03b2c1cc6d40a41b
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2023-02-12 20:17:16 +0300

    Add programm

---
+++ FindLengthStringToArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Clear();
+
+string[] strings = {"MVC", "DRY", "Stack", "GIT", "Android", "ios", "Lunix", "Windows", ":-)", ":-)0"};
+int maxLengthElement = 3;
+
+string[] FindElementInArray(string[] strings, int maxLengthElement)
+{
+    string[] resArray = Array.Empty<string>();
+
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (!(strings[i] is string)) {
+            WriteLine($"Элемент номер {i+1} - {strings[i]} не является строкой!");
+            Environment.Exit(0);
+        }
+        if (strings[i].Length <= maxLengthElement) {
+            Array.Resize(ref resArray, resArray.Length + 1);
+            resArray[resArray.Length - 1] = strings[i];
+        }
+    }
+    return resArray;
+}
+
+WriteLine(string.Join(", ", FindElementInArray(strings, maxLengthElement)));
